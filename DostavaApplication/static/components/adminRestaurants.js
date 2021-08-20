fb7396a856d274219609413cd2f0ab3e252f3c69
Lea Kalmar <kalmar.ra130.2018@uns.ac.rs>
2021-08-20 10:48:37 +0200

    OpenLayer-Leaflet map added

--- DostavaApplication/static/components/adminRestaurants.js
+++ DostavaApplication/static/components/adminRestaurants.js
@@ -1,7 +1,11 @@
 Vue.component("admin-restaurants", {
     data: function() {
         return {
-            restaurantName: ""
+            restaurantName: '',
+            street: '',
+            houseNumber: '',
+            city: '',
+            postalCode: ''
         }
     },
 
@@ -138,12 +142,12 @@ Vue.component("admin-restaurants", {
 			<div class="modal">
 				<div v-on:click="newRestaurantClose" class="close">+</div>
 
-				<div >
+				<div style="display:block;" class="firstStep">
 					<div class="login-title">
 						<h3 style="color: white; font-weight: bolder;"> KREIRAJTE NOVI RESTORAN </h3>
 					</div>
 					
-					<div style="margin-top: 20px;display:block;" class="firstStep">
+					<div style="margin-top: 20px;" >
 						<form>
 							<input v-model="restaurantName" type="text" class="login-inputs" placeholder="Naziv restorana">
 							<label class="error" id="restaurantNameErr" name="labels" display="hidden"> </label>
@@ -194,44 +198,29 @@ Vue.component("admin-restaurants", {
 							</div>
 						</form>
 					</div>
-					<div style="margin-top: 20px;display: none" class="secondStep">
+				</div>
+				
+				<div style="display: none"  class="secondStep">
+					
+					<div class="login-title">
+						<h3 style="color: white; font-weight: bolder;"> LOKACIJA "{{restaurantName}}" </h3>
+					</div>
+					
+					<div style="margin-top: 20px;" >
 						<form>
-							<input v-model="restaurantName" type="text" class="login-inputs" placeholder="Naziv restorana">
-							<label class="error" id="restaurantNameErr" name="labels" display="hidden"> </label>
+							<input  v-model="street" id="street" type="text" class="login-inputs" placeholder="Ulica:">
+							<label class="error" id="streetErr" name="labels" display="hidden"> </label>
 
-							<label style="color: white;display: block;margin:15px 0 0 0;font-weight: bold;">Logo:</label>
-							<input type="file" class="login-inputs" style="margin: 2px auto 2px;" id="inpFile" v-on:change="fileUploaded">
-							<label class="error" id="logoErr" name="labels" display="hidden"> </label>
+							<input  v-model="houseNumber" id="number" type="text" class="login-inputs" placeholder="Broj:">
+							<label class="error" id="houseNumberErr" name="labels" display="hidden"> </label>
 
-							<div class="image-preview" id="imagePreview">
-								<img src="" alt="Image Preview" class="image-preview__image">
-								<span class="image-preview__default-text">Image Preview</span>
-							</div>
-
-							<label style="color: white;display: block;margin:15px 0 0 0;font-weight: bold;">Odaberite tip hrane:</label>
-							<div class="choose-type">
-								<div>
-									<input type="checkbox" id="italian" name="cuisine" value="italian">
-									<label for="italian">Italijanska</label>
-								</div>
-								<div>
-									<input type="checkbox" id="chinese" name="cuisine" value="chinese">
-									<label for="chinese">Kineska</label>
-								</div>
-								<div>
-									<input type="checkbox" id="barbecue" name="cuisine" value="barbecue">
-									<label for="barbecue">Rostilj</label>
-								</div>
-								<div>
-									<input type="checkbox" id="american" name="cuisine" value="american">
-									<label for="american">Americka hrana</label>
-								</div>
-								<div>
-									<input type="checkbox" id="sweets" name="cuisine" value="sweets">
-									<label for="sweets">Poslastice</label>
-								</div>
-							</div>
-							<label class="error" id="logoErr" name="labels" display="hidden"> </label>
+							<input v-model="city" id="city" type="text" class="login-inputs" placeholder="Grad:">
+							<label class="error" id="cityErr" name="labels" display="hidden"> </label>
+							
+							<input v-model="postalCode" id="postalcode" type="text" class="login-inputs" placeholder="Postanski broj:">
+							<label class="error" id="postalCodeErr" name="labels" display="hidden"> </label>
+							
+							<div id="map"></div>
 
 							<div class="steps-div" style="grid-template-columns: 30% 40% 30%;">
 								<button v-on:click="backStep" style="float:right;padding: 10px 20px;margin-top:10px" class="log-btn"> 
@@ -248,7 +237,10 @@ Vue.component("admin-restaurants", {
 							</div>
 						</form>
 					</div>
-					<div style="margin-top: 20px;display: none" class="thirdStep">
+				</div>
+
+				<div style="display: none" class="thirdStep">
+					<div style="margin-top: 20px;" >
 						<form>
 							<input v-model="restaurantName" type="text" class="login-inputs" placeholder="Naziv restorana">
 							<label class="error" id="restaurantNameErr" name="labels" display="hidden"> </label>
@@ -316,7 +308,74 @@ Vue.component("admin-restaurants", {
 	`,
     mounted() {
         window.scrollTo(0, 0);
+        const Map = L.map('map').setView([46.392411189814645, 16.270751953125004], 6);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: apiKey
+        }).addTo(Map);
 
+
+        Map.on('click', function(e) {
+            var coordinate = e.latlng;
+            var lon = coordinate.lng;
+            var lat = coordinate.lat;
+            simpleReverseGeocoding(lon, lat);
+        });
+
+        document.getElementById('street').addEventListener('blur', (event) => {
+            let search = false;
+            let query = 'search?q=';
+            if (this.city) {
+                if (this.street) {
+                    if (this.number) {
+                        query += this.number.trim() + '+';
+                    }
+                    query += this.street.replace(' ', '+').trim() + '%2C+';
+                    search = true;
+                }
+                query += this.city.trim();
+            }
+
+            query += '&format=geojson';
+
+            if (search) {
+                fetch('http://nominatim.openstreetmap.org/' + query).then(function(response) {
+                    return response.json();
+                }).then(function(json) {
+                    L.map('map').setView(json.features[0].geometry.coordinates, 9);
+                    L.marker(json.features[0].geometry.coordinates).addTo(Map);
+                })
+            }
+        });
+
+        document.getElementById('city').addEventListener('blur', (event) => {
+            let search = false;
+            let query = 'search?q=';
+            if (this.city) {
+                if (this.street) {
+                    if (this.number) {
+                        query += this.number.trim() + '+';
+                    }
+                    query += this.street.replace(' ', '+').trim() + '%2C+';
+                    search = true;
+                }
+                query += this.city.trim();
+            }
+
+            query += '&format=geojson';
+
+            if (search) {
+                fetch('http://nominatim.openstreetmap.org/' + query).then(function(response) {
+                    return response.json();
+                }).then(function(json) {
+                    Map.setView([json.features[0].geometry.coordinates[1] + 0.01, json.features[0].geometry.coordinates[0] - 0.015], 14);
+                    L.marker([json.features[0].geometry.coordinates[1], json.features[0].geometry.coordinates[0]]).addTo(Map);
+                })
+            }
+        });
     },
 
     methods: {
@@ -423,4 +482,85 @@ Vue.component("admin-restaurants", {
             document.querySelector('.thirdStep').style.display = 'none';
         }
     }
-})
+});
+
+function simpleReverseGeocoding(lon, lat) {
+    fetch('http://nominatim.openstreetmap.org/reverse?format=json&lon=' + lon + '&lat=' + lat + '&accept-language=sr-Latn').then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        let street = document.getElementById("street");
+        let city = document.getElementById("city");
+        let postcode = document.getElementById("postalcode");
+        let number = document.getElementById("number");
+
+        if (json.address.house_number) {
+            number.value = json.address.house_number;
+            number.dispatchEvent(new Event('input'));
+        } else {
+            number.value = '';
+            number.dispatchEvent(new Event('input'));
+        }
+
+        if (json.address.road) {
+            street.value = json.address.road;
+            street.dispatchEvent(new Event('input'));
+        } else {
+            street.value = '';
+            street.dispatchEvent(new Event('input'));
+        }
+
+        if (json.address.city) {
+            city.value = json.address.city;
+            if (city.value.startsWith('Grad')) {
+                city.value = json.address.city.substring(5);
+            } else if (city.value.startsWith('Opština')) {
+                city.value = json.address.city.substring(8);
+            } else if (city.value.startsWith('Gradska opština')) {
+                city.value = json.address.city.substring(16);
+            }
+            city.dispatchEvent(new Event('input'));
+        } else if (json.address.city_district) {
+            city.value = json.address.city_district;
+            city.dispatchEvent(new Event('input'));
+        } else if (json.address.town) {
+            city.value = json.address.town;
+            city.dispatchEvent(new Event('input'));
+        } else {
+            city.value = '';
+            city.dispatchEvent(new Event('input'));
+        }
+
+        if (json.address.postcode) {
+            postcode.value = json.address.postcode;
+            postcode.dispatchEvent(new Event('input'));
+        } else {
+            postcode.value = '';
+            postcode.dispatchEvent(new Event('input'));
+        }
+    })
+}
+
+
+function simpleGeocoding() {
+    let query = 'search?q=';
+    let number = document.getElementById("number").innerHTML;
+    let street = document.getElementById("street").innerHTML;
+    let city = document.getElementById("city").innerHTML;
+    if (city) {
+        if (street) {
+            if (number) {
+                query += number.trim() + '+';
+            }
+            query += street.replace(' ', '+').trim() + '%2C+';
+        }
+        query += city.trim();
+    }
+
+    query += '&format=geojson';
+
+    fetch('http://nominatim.openstreetmap.org/search?street=' + document.getElementById("number") + '+' + document.getElementById("street") + '&accept-language=sr-Latn').then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        L.marker(json.geometry.coordinates).addTo(Map);
+    })
+}
