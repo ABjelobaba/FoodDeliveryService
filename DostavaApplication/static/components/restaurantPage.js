fc5b49179af2a9a1c9f7ba8eef224f53f10e27a3
Lea Kalmar <kalmar.ra130.2018@uns.ac.rs>
2021-08-22 17:24:22 +0200

    Update restaurantPage.js

--- DostavaApplication/static/components/restaurantPage.js
+++ DostavaApplication/static/components/restaurantPage.js
@@ -72,34 +72,34 @@ Vue.component("restaurantPage", {
                     <h3>Artikli</h3>
                     <h3>Utisci</h3>
                 </div>
-                <div class="nav-menu-rp">
-                <h2 style="text-align: center;" >Kuhinje</h2>
-                <div class="chechbox_types">
-                    <div>
-                        <input type="checkbox" id="processing" name="order-status" value="processing">
-                        <label for="processing">Obrada</label>
+                <div class="nav-menu-rp" style="display: none">
+                    <h2 style="text-align: center;" >Kuhinje</h2>
+                    <div class="chechbox_types">
+                        <div>
+                            <input type="checkbox" id="processing" name="order-status" value="processing">
+                            <label for="processing">Obrada</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="prep" name="order-status" value="prep">
+                            <label for="prep">U pripremi</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="waitingDeliverer" name="order-status" value="waitingDeliverer">
+                            <label for="waitingDeliverer">Ceka dostavljaca</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="transporting" name="order-status" value="transporting">
+                            <label for="transporting">U transportu</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="finished" name="order-status" value="finished">
+                            <label for="finished">Dostavljena</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="canceled" name="order-status" value="canceled">
+                            <label for="canceled">Otkazana</label>
+                        </div>
                     </div>
-                    <div>
-                        <input type="checkbox" id="prep" name="order-status" value="prep">
-                        <label for="prep">U pripremi</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="waitingDeliverer" name="order-status" value="waitingDeliverer">
-                        <label for="waitingDeliverer">Ceka dostavljaca</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="transporting" name="order-status" value="transporting">
-                        <label for="transporting">U transportu</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="finished" name="order-status" value="finished">
-                        <label for="finished">Dostavljena</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="canceled" name="order-status" value="canceled">
-                        <label for="canceled">Otkazana</label>
-                    </div>
-                </div>
                 </div>
             </div>
             <div class="right-container-rp">
