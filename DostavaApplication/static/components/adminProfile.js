312fddd072e3f13caacc92d67162cae444e65b7d
Lea Kalmar <kalmar.ra130.2018@uns.ac.rs>
2021-08-24 12:06:52 +0200

    User Profile

--- DostavaApplication/static/components/adminProfile.js
+++ DostavaApplication/static/components/adminProfile.js
@@ -1,10 +1,17 @@
 Vue.component("admin-profile", {
     data: function() {
         return {
-            name: 'Lea',
-            surname: 'Kalmar',
-            username: 'lea_kalmar',
-            gender: 'ŽENSKO',
+            logedInUser: {
+                name: 'Lea',
+                surname: 'Kalmar',
+                username: 'lea_kalmar',
+                gender: 'ŽENSKO',
+                medal: 'bronze',
+                birthDate: "1999-04-23",
+                role: 'user',
+                points: 3456,
+                address: 'Zelengorska 27, Subotica'
+            },
             oldPassword: '',
             newPassword: ''
         }
@@ -16,33 +23,53 @@ Vue.component("admin-profile", {
 
         <div class="float-left-div" style="position:relative; top:0;">
             <div class="restaurant-types">
-                <img class="user-img" src="images/user-img.png">
-                <h2 style="text-align: center; margin:0">Lea Kalmar</h2>
+                <img v-if="logedInUser.medal == 'gold'" class="user-img" src="images/gold.png" alt="User">
+                <img v-if="logedInUser.medal == 'silver'" class="user-img" src="images/silver.png" alt="User">
+                <img v-if="logedInUser.medal == 'bronze'" class="user-img" src="images/bronze.png" alt="User">
+                <h2 style="text-align: center; margin:0">{{logedInUser.name}} {{logedInUser.surname}}</h2>
+            </div>
+
+            <div class="restaurant-types" v-bind:id="logedInUser.medal">
+                <h3 style="text-align: center; margin:0 0 5% 0">Sakupljeni bodovi</h3>
+                <h2 style="text-align: center; margin:0">{{logedInUser.points}}</h2>
             </div>
         </div>
 
         <div style="width:100%">
+            <div class="user-info">
+                <h3>Adresa za dostavu</h3>
+                <form>
+                    <div class="inputs-div">
+                        <input v-model="logedInUser.address" type="text" class="profile-change-input" style="width:auto" >
+                        <label class="error" id="nameErr" name="labels" display="hidden"> </label>
+                    </div>
+
+                    <button style=" margin: 20px auto;width:280px" class="black-btn"> Potvrdi</button>
+                </form>
+
+            </div>
+
             <div class="user-info">
                 <h3>Vaši podaci</h3>
                 <form>
 
                     <div class="inputs-div">
                         <label class="input-label">Ime:</label>
-                        <input v-model="name" type="text" class="profile-change-input">
+                        <input v-model="logedInUser.name" type="text" class="profile-change-input" >
                         <label class="error" id="nameErr" name="labels" display="hidden"> </label>
                     </div>
                     <div class="inputs-div">
                         <label class="input-label">Prezime:</label>
-                        <input v-model="surname" type="text" class="profile-change-input" placeholder="Prezime">
+                        <input v-model="logedInUser.surname" type="text" class="profile-change-input" placeholder="Prezime">
                         <label class="error" id="surnameErr" name="labels" display="hidden"> </label>
                     </div>
                     <div class="inputs-div">
                         <label class="input-label">Korisnicko ime:</label>
-                        <input disabled v-model="username" type="text" class="profile-change-disabled" placeholder="Korisničko ime">
+                        <input disabled v-model="logedInUser.username" type="text" class="profile-change-disabled" placeholder="Korisničko ime">
                     </div>
                     <div class="inputs-div">
                         <label class="input-label">Pol:</label>
-                        <select v-model="gender" class="profile-change-select">
+                        <select v-model="logedInUser.gender" class="profile-change-select">
                                 <option hidden>Odaberite pol..</option>
                                 <option>MUŠKO</option>
                                 <option>ŽENSKO</option>
@@ -51,7 +78,7 @@ Vue.component("admin-profile", {
                     </div>
                     <div class="inputs-div">
                         <label class="input-label">Datum rođenja:</label>
-                        <input type="date" class="profile-change-input" style="margin-top: 1px;" id="date_input">
+                        <input v-model="logedInUser.birthDate" type="date" class="profile-change-input" style="margin-top: 1px;" id="date_input">
                         <label class="error" id="dateErr" name="labels" display="hidden"> </label>
                     </div>
 
