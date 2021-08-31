1a8b37f5b8e1b2acee598e145c81d05d00c3ea48
Lea Kalmar <kalmar.ra130.2018@uns.ac.rs>
2021-08-31 17:19:06 +0200

    Remove shopping cart from customer

--- DostavaApplication/src/beans/Customer.java
+++ DostavaApplication/src/beans/Customer.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class Customer extends User {
 	private List<Order> allOrders;
-	private ShoppingCart cart;
 	private int totalPoints;
 	private CustomerCategory category;
 	private String deliveryAddress;
@@ -15,7 +14,6 @@ public class Customer extends User {
 			Role role) {
 		super(username, password, name, surname, gender, birthdate, role);
 		this.allOrders = new ArrayList<Order>();
-		this.cart = new ShoppingCart(username);
 		this.totalPoints = 0;
 		this.category = new CustomerCategory();
 		this.deliveryAddress = "";
@@ -27,14 +25,6 @@ public class Customer extends User {
 
 	public void setAllOrders(List<Order> allOrders) {
 		this.allOrders = allOrders;
-	}
-
-	public ShoppingCart getCart() {
-		return cart;
-	}
-
-	public void setCart(ShoppingCart cart) {
-		this.cart = cart;
 	}
 
 	public int getTotalPoints() {
